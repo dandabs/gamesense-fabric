a781edd323990c0036f63e2f11d18f25b4d821de
Daniel Adams <danstanswinxp@gmail.com>
2021-12-26 01:21:21 +0000

    um

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,6 +1,15 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,6 +18,9 @@ public class ExampleMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("modid");
+	private static final JsonParser JSON_PARSER = new JsonParser();  
+
+	public String ADDRESS = "";
 
 	@Override
 	public void onInitialize() {
@@ -17,6 +29,23 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		String path = "/Library/Application Support/SteelSeries Engine 3/coreProps.json";
+		File file = new File(path);
+		String fileData;
+		try {
+
+			fileData = Files.toString(file, Charsets.UTF_8);
+			JsonObject json = (JsonObject) JSON_PARSER.parse(fileData);
+
+			ADDRESS = json.get("address").getAsString();
+
+			LOGGER.info(ADDRESS);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 }
